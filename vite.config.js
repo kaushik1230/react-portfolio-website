1fb9ecfda6d87f99966ccdd8c7a2e849a7ea79ea
kaushik Dutta <kaushik199906@gmail.com>
2025-03-01 17:12:14 +0530

    Updated project with new changes3

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(() => {
-  return {
-    build: {
-      outDir: "dist",
-    },
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: "dist", // Ensure this is set correctly
+    chunkSizeWarningLimit: 2000, // Increase from default 500 KB
+  },
 });
